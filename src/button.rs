99f21c7056e0c4c1e8e9f6b55aa0d4128fb3fe60
9theyab <143641555+9theyab@users.noreply.github.com>
2024-10-09 15:39:54 -0400

    button.rs

--- src/button.rs
+++ src/button.rs
@@ -2,7 +2,6 @@ use bevy::prelude::*;
 use crate::resources::*;
 use crate::fishingView::*;
 
-
 const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
 const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
 const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
@@ -10,49 +9,69 @@ const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
 #[derive(Component)]
 pub struct ButtonVisible(pub bool);
 
+
 pub fn button_system(
-    mut interaction_query: Query<
-        (
-            &Interaction,
-            &mut BackgroundColor,
-            &mut BorderColor,
-            &mut Visibility,
-            &Children,
-        ),
-        (Changed<Interaction>, With<Button>),
-    >,
-    mut text_query: Query<&mut Text>,
+    input: Res<ButtonInput<KeyCode>>,   
+    mut button_query: Query<(&mut BackgroundColor, &mut BorderColor, &Children), (With<Button>,)>,
+    mut visibility_query: Query<&mut Visibility, With<Button>>, 
+    mut text_query: Query<&mut Text>, 
     mut start_fishing_animation: ResMut<StartFishingAnimation>,
-    mut fishing_timer: ResMut<FishingAnimationDuration>,      
-    mut next: ResMut<NextState<FishingMode>>,
-    state: Res<State<FishingMode>>  
+    mut fishing_timer: ResMut<FishingAnimationDuration>,
+    mut next_state: ResMut<NextState<FishingMode>>,  
+    state: Res<State<FishingMode>>,  
 ) {
-    for (interaction, mut color, mut border_color, mut visibility, children) in &mut interaction_query {
+   
+    for (mut color, mut border_color, children) in &mut button_query {
+        let mut visibility = visibility_query.single_mut();
         let mut text = text_query.get_mut(children[0]).unwrap();
-        match *interaction {
-            Interaction::Pressed => {
-                text.sections[0].value = "Throw Rod".to_string();
-                *color = PRESSED_BUTTON.into();
+
+        
+        if *visibility == Visibility::Visible || state.eq(&FishingMode::Fishing) {
+
+            
+            if state.eq(&FishingMode::Overworld) {
+                text.sections[0].value = "Throw Rod(X)".to_string(); 
+            } else if state.eq(&FishingMode::Fishing) {
+                text.sections[0].value = "Exit(X)".to_string(); 
+            }
+
+            
+            if input.pressed(KeyCode::KeyX) {
+                *color = HOVERED_BUTTON.into();  
+                border_color.0 = Color::WHITE;
+            } else {
+                *color = NORMAL_BUTTON.into();  
+                border_color.0 = Color::BLACK;
+            }
+
+            
+            if input.just_pressed(KeyCode::KeyX) && state.eq(&FishingMode::Overworld) {
+                *color = PRESSED_BUTTON.into();  
                 start_fishing_animation.active = true;
                 start_fishing_animation.button_control_active = false;
                 fishing_timer.0.reset();
-                *visibility = Visibility::Hidden;
-                next.set(state.get().next()); 
 
-            }
-            Interaction::Hovered => {
-                text.sections[0].value = "Throw Rod".to_string();
-                *color = HOVERED_BUTTON.into();
-                border_color.0 = Color::WHITE;
-            }
-            Interaction::None => {
-                text.sections[0].value = "Throw Rod".to_string();
-                *color = NORMAL_BUTTON.into();
-                border_color.0 = Color::BLACK;
+                
+                next_state.set(FishingMode::Fishing);
+                println!("Switching to fishing mode");
+
+            
+            } else if input.just_pressed(KeyCode::KeyX) && state.eq(&FishingMode::Fishing) {
+                println!("Exiting fishing mode");
+                *color = NORMAL_BUTTON.into();  
+                start_fishing_animation.active = false;
+                start_fishing_animation.button_control_active = true;
+                *visibility = Visibility::Visible;  
+
+                
+                next_state.set(FishingMode::Overworld);
+                println!("Switching to overworld mode");
             }
         }
     }
 }
+
+
 
 pub fn spawn_button(commands: &mut Commands, asset_server: Res<AssetServer>) {
     commands
@@ -88,7 +107,7 @@ pub fn spawn_button(commands: &mut Commands, asset_server: Res<AssetServer>) {
                 })
                 .with_children(|parent| {
                     parent.spawn(TextBundle::from_section(
-                        "Throw Rod",
+                        "Throw Rod(X)",
                         TextStyle {
                             font: asset_server.load("pixel.ttf"),
                             font_size: 40.0,
@@ -98,3 +117,4 @@ pub fn spawn_button(commands: &mut Commands, asset_server: Res<AssetServer>) {
                 });
         });
 }
+
